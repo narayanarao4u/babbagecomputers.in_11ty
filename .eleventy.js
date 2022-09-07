0074f08de6dafb07b22b16c840e23164dee94950
Narayana Rao <narayanarao4u@yahoo.com>
2022-09-07 19:54:17 +0530

    Uploaded to Hioxindia

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,14 @@
 module.exports = function(eleventyConfig){
-    eleventyConfig.addPassthroughCopy('./src/favicon.ico');
-    eleventyConfig.addPassthroughCopy('./src/assests');
-    eleventyConfig.addPassthroughCopy('./src/BCTLOGO.png');
+    eleventyConfig.addPassthroughCopy('src/favicon.ico');
+    eleventyConfig.addPassthroughCopy('src/assests');
+    eleventyConfig.addPassthroughCopy('src/BCTLOGO.png');
+    eleventyConfig.addPassthroughCopy('src/googlea1f1d7a9e8a26702.html');
     // Return your Object options:
     return {
         dir: {
           input: 'src',
           includes: '_includes',
-          output: '_site',
+          output: 'dist',
         },
         templateFormats: ['md', 'njk', 'html'],
         markdownTemplateEngine: 'njk',
